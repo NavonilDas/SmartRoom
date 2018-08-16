5fc7401f16c137be7831db12825525b984a1db99
NavonilDas <navneeladas@gmail.com>
2018-08-17 00:26:03 +0530

    Added the Android App

--- ESP8266/ESP8266.ino
+++ ESP8266/ESP8266.ino
@@ -6,15 +6,16 @@
 const char* ssid = "EspServer";
 const char* password = "12345678";
 const char* mqtt_server = "iot.eclipse.org";
-
+// 9EB1928E ---> In
+// DEDE09CB ---> out
 WiFiClient espClient;
 PubSubClient client(espClient);
 long lastMsg = 0;
 char msg[50];
 int value = 0;
+short check = 0;
 
 void setup_wifi() {
-
   delay(10);
   // We start by connecting to a WiFi network
   Serial.println();
@@ -37,39 +38,57 @@ void setup_wifi() {
 }
 
 void callback(char* topic, byte* payload, unsigned int length) {
-  Serial.print("Message arrived [");
-  Serial.print(topic);
-  Serial.print("] ");
-  for (int i = 0; i < length; i++) {
-    Serial.print((char)payload[i]);
-  }
-  Serial.println();
-
-  // Switch on the LED if an 1 was received as first character
-  if ((char)payload[0] == '1') {
-    digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
-    // but actually the LED is on; this is because
-    // it is acive low on the ESP-01)
-  } else {
-    digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
-  }
-
+  char c = (char)payload[0];
+  /// Switch On or Off The Relay
+  // 0,1,2,3 -> Relay On
+  // 4,5,6,7 -> Relay off
+  
+  switch(c){
+      case '0':
+        digitalWrite(D1,LOW);
+       break;
+      case '1':
+        digitalWrite(D2,LOW);
+       break;
+      case '2':
+        digitalWrite(D3,LOW);
+       break;
+      case '3':
+        digitalWrite(D4,LOW);
+       break;
+      case '4':
+        digitalWrite(D1,HIGH);
+       break;
+      case '5':
+        digitalWrite(D2,HIGH);
+       break;
+      case '6':
+        digitalWrite(D3,HIGH);
+       break;
+      case '7':
+        digitalWrite(D4,HIGH);
+       break;
+      default:
+      Serial.println("Not Found Value !");      
+    }
 }
 
 void reconnect() {
   // Loop until we're reconnected
   while (!client.connected()) {
+   
     Serial.print("Attempting MQTT connection...");
+    
     // Create a random client ID
-    String clientId = "ESP8266Client-";
+    
+    String clientId = "ESP8266Client";
     clientId += String(random(0xffff), HEX);
+
     // Attempt to connect
     if (client.connect(clientId.c_str())) {
       Serial.println("connected");
-      // Once connected, publish an announcement...
-      client.publish("outTopic", "hello world");
-      // ... and resubscribe
-      client.subscribe("inTopic");
+      client.publish("DEDE09CB", "Connected");
+      client.subscribe("9EB1928E");
     } else {
       Serial.print("failed, rc=");
       Serial.print(client.state());
@@ -81,8 +100,18 @@ void reconnect() {
 }
 
 void setup() {
-  pinMode(BUILTIN_LED, OUTPUT);     // Initialize the BUILTIN_LED pin as an output
   Serial.begin(115200);
+  ////// Setting Output Pins
+  pinMode(D1,OUTPUT);
+  pinMode(D2,OUTPUT);
+  pinMode(D3,OUTPUT);
+  pinMode(D4,OUTPUT);
+  //// Setting ALL values to High
+  digitalWrite(D1,HIGH);
+  digitalWrite(D2,HIGH);
+  digitalWrite(D3,HIGH);
+  digitalWrite(D4,HIGH);
+
   setup_wifi();
   client.setServer(mqtt_server, 1883);
   client.setCallback(callback);
@@ -91,17 +120,12 @@ void setup() {
 void loop() {
 
   if (!client.connected()) {
+    check = 0;
     reconnect();
   }
   client.loop();
-
-  long now = millis();
-  if (now - lastMsg > 2000) {
-    lastMsg = now;
-    ++value;
-    snprintf (msg, 75, "hello world #%ld", value);
-    Serial.print("Publish message: ");
-    Serial.println(msg);
-    client.publish("outTopic", msg);
+  if(check == 0){
+    client.publish("DEDE09CB", "Connected");
+    check = 1;
   }
 }
---
+++ RoomController/app/src/main/java/com/wificontroller/navonildas/roomcontroller/MainActivity.java
@@ -0,0 +1,27 @@
+package com.wificontroller.navonildas.roomcontroller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    ListView rooms;
+    String[] roomsLabel = {"Room 1","Room 2","Room 3"};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        rooms = findViewById(R.id.roomsLv);
+        rooms.setAdapter(new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,roomsLabel));
+        rooms.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this, ""+i, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
