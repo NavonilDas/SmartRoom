5d093c650e3087223230f4d315d2aae926143267
NavonilDas <navneeladas@gmail.com>
2018-08-17 02:43:37 +0530

    Completed With App

--- RoomController/app/src/main/java/com/wificontroller/navonildas/roomcontroller/Adapters/RoomAdapter.java
+++ RoomController/app/src/main/java/com/wificontroller/navonildas/roomcontroller/Adapters/RoomAdapter.java
@@ -1,4 +1,86 @@
 package com.wificontroller.navonildas.roomcontroller.Adapters;
 
-public class RoomAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import com.wificontroller.navonildas.roomcontroller.R;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+import java.io.UnsupportedEncodingException;
+
+public class RoomAdapter extends BaseAdapter {
+    String[] items;
+    short[] toggle;
+    LayoutInflater inflater;
+    MqttAndroidClient client;
+
+    public RoomAdapter(String[] items, Context ctx, MqttAndroidClient client) {
+        this.items = items;
+        this.client = client;
+        inflater = (LayoutInflater) ctx.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        toggle = new short[items.length];
+        for (int i = 0; i < items.length; i++)
+            toggle[i] = 0;
+    }
+
+    @Override
+    public int getCount() {
+        return items.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return items[i];
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        View vi = inflater.inflate(R.layout.room_item, null);
+        TextView tv = vi.findViewById(R.id.title);
+        final Switch sv = vi.findViewById(R.id.onoff);
+        tv.setText(
+                items[i]
+        );
+        final int itemInd = i;
+
+        sv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                byte[] encodedPayload = new byte[0];
+                MqttMessage message;
+                String payload = "";
+                if (sv.isChecked()) {
+                    /// on
+                    sv.setText("ON");
+                    payload = ""+ itemInd;
+
+                } else {
+                    // off
+                    sv.setText("OFF");
+                    payload = ""+ (4+itemInd);
+                }
+                try {
+                    encodedPayload = payload.getBytes("UTF-8");
+                    message = new MqttMessage(encodedPayload);
+                    client.publish("9EB1928E", message);
+                } catch (UnsupportedEncodingException | MqttException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        return vi;
+    }
 }
--- RoomController/app/src/main/java/com/wificontroller/navonildas/roomcontroller/RoomActivity.java
+++ RoomController/app/src/main/java/com/wificontroller/navonildas/roomcontroller/RoomActivity.java
@@ -6,13 +6,112 @@ import android.os.Bundle;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.wificontroller.navonildas.roomcontroller.Adapters.RoomAdapter;
+
+import org.eclipse.paho.android.service.MqttAndroidClient;
+import org.eclipse.paho.client.mqttv3.IMqttActionListener;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttToken;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
 public class RoomActivity extends AppCompatActivity {
     ListView roomLv;
+    String[] items = {"LED", "Fan", "TubeLight", "Night Bulb"};
+    String clientId;
+    MqttAndroidClient client;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_room);
-        Intent room = getIntent();
-        int x = room.getIntExtra("ROOM_NO",0);
+//        Intent room = getIntent();
+//        int x = room.getIntExtra("ROOM_NO",0);
+        roomLv = findViewById(R.id.roomLv);
+        clientId = MqttClient.generateClientId();
+        client =
+                new MqttAndroidClient(getApplicationContext(), "tcp://iot.eclipse.org:1883",
+                        clientId);
+        try {
+            IMqttToken token = client.connect();
+            token.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    // We are connected
+                    Toast.makeText(RoomActivity.this, "Connected", Toast.LENGTH_SHORT).show();
+                    Subscribe();
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+                    // Something went wrong e.g. connection timeout or firewall problems
+                    Toast.makeText(RoomActivity.this, "Failed Connection", Toast.LENGTH_SHORT).show();
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+        client.setCallback(new MqttCallback() {
+            @Override
+            public void connectionLost(Throwable throwable) {
+                Toast.makeText(RoomActivity.this, "Lost Connection", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void messageArrived(String s, MqttMessage mqttMessage) throws Exception {
+                Toast.makeText(RoomActivity.this, new String(mqttMessage.getPayload()), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void deliveryComplete(IMqttDeliveryToken iMqttDeliveryToken) {
+//                Toast.makeText(RoomActivity.this, "Message Delivered", Toast.LENGTH_SHORT).show();
+            }
+        });
+        roomLv.setAdapter(new RoomAdapter(items, this, client));
+    }
+
+    public void Subscribe() {
+        int qos = 1;
+        try {
+            IMqttToken subToken = client.subscribe("DEDE09CB", qos);
+            subToken.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    Toast.makeText(RoomActivity.this, "Subscribed", Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken,
+                                      Throwable exception) {
+                    Toast.makeText(RoomActivity.this, "Error in Subscription", Toast.LENGTH_SHORT).show();
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        try {
+            IMqttToken disconToken = client.disconnect();
+            disconToken.setActionCallback(new IMqttActionListener() {
+                @Override
+                public void onSuccess(IMqttToken asyncActionToken) {
+                    Toast.makeText(RoomActivity.this, "Disconnected", Toast.LENGTH_SHORT).show();
+                }
+
+                @Override
+                public void onFailure(IMqttToken asyncActionToken,
+                                      Throwable exception) {
+                    Toast.makeText(RoomActivity.this, "Cann't Disconnect", Toast.LENGTH_SHORT).show();
+                }
+            });
+        } catch (MqttException e) {
+            e.printStackTrace();
+        }
     }
 }
